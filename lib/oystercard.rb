351db96128d71722956f11ab686cc62e41f11d57
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-04-06 17:39:41 +0100

    Updated Errors and max limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,15 @@
-class Oystercard
+require 'errors'
 
+class Oystercard
   attr_reader :balance
+  MAXIMUM_LIMIT = 90
   
   def initialize
     @balance = 0
   end
 
   def top_up(value)
+    raise ValueError if balance + value > MAXIMUM_LIMIT
     @balance += value
   end
-
 end
