642db0653acc119578dda1fa99e3274e88f9eb10
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-04-06 15:57:36 +0100

    Stage 1 completed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,16 @@
+require 'oystercard'
+
 describe Oystercard do
+  it 'allows the user to create an instance of Oystercard' do
+    is_expected.to be_instance_of Oystercard
+  end
+
+  it { is_expected.to respond_to :balance }
+
+  describe '#balance' do
+    it 'returns a default balance of zero' do
+      expect(subject.balance).to eq 0
+    end
+  end
+    
 end
