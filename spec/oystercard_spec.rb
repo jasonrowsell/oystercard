f0a448f2c716c0096e0b906ccf4c7741f0ed5e52
Ionut Cristian Mintar <ionutcristian_mintar@yahoo.com>
2021-04-06 16:57:30 +0100

    Added 'top up' functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,5 +12,13 @@ describe Oystercard do
       expect(subject.balance).to eq 0
     end
   end
+  
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+
+  describe '#top up' do 
+    it 'increases the balance' do
+      expect{ subject.top_up 5 }.to change{ subject.balance }.by 5
+    end
+  end
     
 end
